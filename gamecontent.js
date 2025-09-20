f2bad86dcd0e6e4fe36de39c11c29ea3745fda9c
SkeletalVirus <61529324+SkeletalVirus@users.noreply.github.com>
2025-09-20 19:40:40 -0400

    Update gamecontent.js

--- gamecontent.js
+++ gamecontent.js
@@ -1,4 +1,4 @@
-const baseURL = 'https://skeletalvirus.github.io/lyricguesser'
+const baseURL = 'https://skeletalvirus.github.io/lyricguesser/'
 // const albumList = document.getElementById("albumList")
 let gameData = {artists: []}
 let savedContent = JSON.parse(localStorage.getItem('savedContent'))
@@ -188,4 +188,5 @@ function loadData() {
         })
 
         console.log(total)
+
     }
